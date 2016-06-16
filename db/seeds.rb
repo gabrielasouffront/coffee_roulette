bb88068c468bc8c756bb1294b2bb6142ea507a48
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-16 08:34:27 -0400

    Add seed data for 5 rand dept, and 70 rand employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Employee.destroy_all
+Department.destroy_all
 
 # create departments
-engineering = Department.create(name:"Engineering")
-online_education = Department.create(name:"Online Education")
-in_person_education = Department.create(name:"In Person Education")
-admissions = Department.create(name:"Admissions")
-curriculum = Department.create(name:"Curriculum")
+5.times do
+  FactoryGirl.create(:department)
+end
+70.times do
+  rand_dept = Department.all.sample
+  FactoryGirl.create(:employee, department: rand_dept)
+end
+# engineering = Department.create(name:"Engineering")
+# online_education = Department.create(name:"Online Education")
+# in_person_education = Department.create(name:"In Person Education")
+# admissions = Department.create(name:"Admissions")
+# curriculum = Department.create(name:"Curriculum")
 
 # create some employees
-avidor = Employee.create(name:"Avidor", email:"avi@test.com", department:online_education)
-antoin = Employee.create(name:"Antoin", email:"antoin@test.com", department:in_person_education)
+# avidor = Employee.create(name:"Avidor", email:"avi@test.com", department:online_education)
+# antoin = Employee.create(name:"Antoin", email:"antoin@test.com", department:in_person_education)
