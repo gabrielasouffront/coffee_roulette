1ef9a3ddccc4c23aa24bebbde765b6a3d51665d4
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-16 19:04:07 -0400

    Add previous_coffee_mates method to employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,12 @@
 class Employee < ActiveRecord::Base
   belongs_to :department
   has_many :guest_lists
+  has_many :employees, through: :guest_lists
   has_many :coffee_dates, through: :guest_lists
   has_many :months, through: :coffee_dates
+
+  def previous_coffee_mates
+    emps = coffee_dates.collect { | coffee_date | coffee_date.employees }
+    emps.flatten.tap { | e | e.delete(self) }
+  end
 end
