46658f8b68f4dfd77840a3425805885f92a2f143
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-18 10:29:49 -0400

    Logic to build coffee dates working

--- app/services/pairing_service.rb
+++ app/services/pairing_service.rb
@@ -30,18 +30,14 @@ class PairingService
     the_rest = @unpaired_employees - [emp1]
     @unpaired_employees.count > 1 &&
     the_rest - emp1.previous_coffee_mates != [] &&
-    emp1.department != emp2.department
+    the_rest.any?{ | emp | emp.department != emp1.department }
   end
 
   def pair_everyone_up
-    # count = 0
     while pairs_are_possible?
       check_a_pair
-      # count += 1
-      # binding.pry if count > 2000
     end
     if @unpaired_employees.count > 1
-      puts "OH SNAP, GONNA HAVE TO DO THIS AGAIN!"
       @month.coffee_dates.destroy_all
       @unpaired_employees = Employee.all
       pair_everyone_up
