dfe926f4f206a2fb461b6942714df6d910b878be
antoinfive <antoinfive@gmail.com>
2016-07-21 16:34:21 -0400

    add parser to add employees from csv file

---
+++ app/services/roster_updater.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+class RosterUpdater
+
+  def self.build_roster
+    CSV.foreach('lib/fixtures/employee_roster.csv', headers: :true) do |row|     
+     Employee.find_or_create_by(name: row['First Name'] + " " + row['Last Name'], email: row['Email'])
+   end
+  end
+
+end
