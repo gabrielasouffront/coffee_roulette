cfb3559c2644118e406d2ebb6fc8e81cc373a3a6
antoinfive <antoinfive@gmail.com>
2016-07-22 14:44:44 -0400

    add departments to each employee and made some changes to coffee pairing email document'

--- app/services/roster_updater.rb
+++ app/services/roster_updater.rb
@@ -3,8 +3,9 @@ require 'csv'
 class RosterUpdater
 
   def self.build_roster
-    CSV.foreach('lib/fixtures/employee_roster.csv', headers: :true) do |row|     
-     Employee.find_or_create_by(name: row['First Name'] + " " + row['Last Name'], email: row['Email'])
+    CSV.foreach('lib/fixtures/employee_roster.csv', headers: :true) do |row|       
+     department = Department.find_by(name: row['Department'])
+     Employee.find_or_create_by(name: row['First Name'] + " " + row['Last Name'], email: row['Email'], department: department)
    end
   end
 
