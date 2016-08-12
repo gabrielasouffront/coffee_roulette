a635eccfa0c7239d08dd03a0fa603aabfeba1009
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-08-12 15:02:30 -0400

    Change Roster Updater to find_or_create_by depatrment

--- app/services/roster_updater.rb
+++ app/services/roster_updater.rb
@@ -4,7 +4,7 @@ class RosterUpdater
 
   def self.build_roster
     CSV.foreach('lib/fixtures/employee_roster.csv', headers: :true) do |row|       
-     department = Department.find_by(name: row['Department'])
+     department = Department.find_or_create_by(name: row['Department'])
      Employee.find_or_create_by(name: row['First Name'] + " " + row['Last Name'], email: row['Email'], department: department)
    end
   end
